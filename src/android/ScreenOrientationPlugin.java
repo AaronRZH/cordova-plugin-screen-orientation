e29a416c2adfd113ad8a254a0758cb652f3cc7b2
aaronrzh <287628769@qq.com>
2017-04-16 02:47:38 +0800

    save last orientation type

--- src/android/ScreenOrientationPlugin.java
+++ src/android/ScreenOrientationPlugin.java
@@ -6,6 +6,7 @@ import org.apache.cordova.LOG;
 import android.content.Context;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import android.content.SharedPreferences;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
@@ -15,6 +16,7 @@ import android.util.Log;
 
 public class ScreenOrientationPlugin extends CordovaPlugin {
     private static final String LOG_TAG = "ScreenOrientationPlugin";
+    private static final String SP_Key = "orientationType";
 
     @Override
     public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
@@ -26,22 +28,44 @@ public class ScreenOrientationPlugin extends CordovaPlugin {
         return true;
     }
 
+    @Override
+    protected void pluginInitialize() {
+        Activity activity = cordova.getActivity();
+        SharedPreferences sp = activity.getSharedPreferences(LOG_TAG, Activity.MODE_PRIVATE);
+        String type = sp.getString(SP_Key, null);
+        if (type != null) {
+            setOrientation(type);
+        }
+    }
+
     private void setOrientation(String orientation) {
         Activity activity = cordova.getActivity();
+        boolean flag = false;
         if (orientation.equalsIgnoreCase("any")) {
             activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+            flag = true;
         } else if (orientation.equalsIgnoreCase("portrait")) {
             activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+            flag = true;
         } else if (orientation.equalsIgnoreCase("portrait-primary")) {
             activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            flag = true;
         } else if (orientation.equalsIgnoreCase("portrait-secondary")) {
             activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+            flag = true;
         } else if (orientation.equalsIgnoreCase("landscape")) {
             activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+            flag = true;
         } else if (orientation.equalsIgnoreCase("landscape-primary")) {
             activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            flag = true;
         } else if (orientation.equalsIgnoreCase("landscape-secondary")) {
             activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+            flag = true;
+        }
+        if (flag) {
+            SharedPreferences sp = activity.getSharedPreferences(LOG_TAG, Activity.MODE_PRIVATE);
+            sp.edit().putString(SP_Key, orientation).commit();
         }
     }
 }
